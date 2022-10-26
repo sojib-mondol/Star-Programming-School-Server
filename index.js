61b98768cbeb63fe1013a4db2a98e39c8b32c977
sojib-sudo <sojibmandol@gmail.com>
2022-10-26 20:34:47 +0600

    cors added in server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send("Star programming school api runnig");
